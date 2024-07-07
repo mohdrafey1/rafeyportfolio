4c15c3edf14697a74796db3477308a259097d068
Mohd Rafey <mohdrafey600@gmail.com>
2024-07-07 14:01:35 +0530

    vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import About from './components/About/About';
 import Projects from './components/Projects/Projects';
 import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 function App() {
     return (
@@ -15,6 +17,8 @@ function App() {
             <Projects />
             <Contact />
             <Footer />
+            <Analytics />
+            <SpeedInsights />
         </div>
     );
 }
