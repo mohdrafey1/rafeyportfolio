30b284f7ed17d5f795a030892a28b40d4e7af4ff
Mohd Rafey <mohdrafey600@gmail.com>
2024-08-06 13:13:14 +0530

    corrected classname

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -46,9 +46,9 @@ function Header() {
                         viewBox="0 0 24 24"
                         fill="none"
                         stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                         className="tabler-icon tabler-icon-menu"
                     >
                         <path d="M3 12h18"></path>
@@ -70,9 +70,9 @@ function Header() {
                         viewBox="0 0 24 24"
                         fill="none"
                         stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                         className="tabler-icon tabler-icon-x"
                     >
                         <path d="M18 6L6 18"></path>
